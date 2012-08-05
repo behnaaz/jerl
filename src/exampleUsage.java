46ffebe8e3f90825d607599539a8ad2bc730bad2
17michaelt <17michaelt@gmail.com>
2012-08-05 15:56:12 +0000

    Fixed file after review

--- src/exampleUsage.java
+++ src/exampleUsage.java
@@ -12,13 +12,10 @@ public class exampleUsage {
     	final Runtime rt1, rt2, rt3;
     	rt1 = (Runtime) Class.forName("jerl").newInstance();
     	rt2 = (Runtime) Class.forName("jerl").newInstance();
-    	//TODO: use rt3 for some jerl shmem
     	rt3 = (Runtime) Class.forName("jerl").newInstance();
 
-    	// Needs to be compiled as part of nested VM
+    	// Get the IO from the JERL Instance
     	Runtime.InputOutputStreamFD b = (Runtime.InputOutputStreamFD) rt3.getFD(UsermodeConstants.STDOUT_FILENO);
-    	
-    	// Needs to be compiled as part of nested VM
     	Runtime.InputOutputStreamFD d = (Runtime.InputOutputStreamFD) rt3.getSTDOutputFD();
 
 
@@ -36,7 +33,7 @@ public class exampleUsage {
     	rt3.start(new String[]{"jerl","-e {print qq(PerlVersion: $] \\n)}"});
     	rt3.execute();
     	System.out.println("FD:string-->"+b.getOutString()+"<--rt3");
-    	System.out.println("STDOut:string-->"+b.getOutString()+"<--rt3");
+    	System.out.println("STDOut:string-->"+d.getOutString()+"<--rt3");
 
     	
     }
